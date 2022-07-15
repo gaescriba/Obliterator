c81efc95e9f397be166c9c6e652ab817f7eac2aa
gaescriba <58343999+gaescriba@users.noreply.github.com>
2022-07-15 19:54:59 -0400

    addef 60-30-10 color references

--- cliento/windi.config.js
+++ cliento/windi.config.js
@@ -2,5 +2,12 @@ import typhography from 'windicss/plugin/typography'
 
 export default {
   darkMode: 'class',
-  plugins: [typhography({ dark: true })]
+  plugins: [typhography({ dark: true })],
+  theme: {
+    colors: {
+      sixty : "#191716",
+      thirty : "#090C9B",
+      ten : "#99FFCC"
+    }
+  }
 }
