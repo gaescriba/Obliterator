67b72965d82be4db74584328cd16ae8482765579
llanquitru <llanquitrusandoval@gmail.com>
2022-07-15 05:48:59 -0400

    Implementacion de boostrap

--- client/src/routes/Session/Register.svelte
+++ client/src/routes/Session/Register.svelte
@@ -32,7 +32,46 @@
     }
 </script>
 
-<div class="Register">
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</svelte:head>
+
+<form>
+    <!-- Email input -->
+    <div class="form-outline mb-4">
+      <label class="form-label" for="form2Example1">Nombre de Usuario</label>
+      <input type="text" id="form2Example1" class="form-control" />
+     
+    </div>
+    <div class="form-outline mb-4">
+        <label class="form-label" for="form2Example2">Correo</label>
+        <input type="email" id="form2Example2" class="form-control" />
+       
+      </div>
+  
+    <!-- Password input -->
+    <div class="form-outline mb-4">
+      <label class="form-label" for="form2Example2">Contraseña</label>
+      <input type="password" id="form2Example2" class="form-control" />
+     
+    </div>
+    
+    <div class="form-outline mb-4">
+        <label class="form-label" for="form2Example2">Repetir contraseña</label>
+        <input type="password" id="form2Example2" class="form-control" />
+       
+      </div>
+   
+    <button type="submit" class="btn btn-primary">Registrarse</button>
+   
+  </form>
+
+
+
+
+
+
+<!-- <div class="Register">
     <p>Nombre de usuario</p>
     <input type="text" id="username" placeholder={ph_username}>
     <p>Correo electronico</p>
@@ -50,4 +89,4 @@
     {#if blankEntries}
         <p>Existen campos en blanco</p>
     {/if}
-</div>
+</div> -->
---
+++ client/src/routes/sell/sellRegister.svelte
@@ -0,0 +1,37 @@
+<script>
+ import axios from 'axios'
+
+let ph_model_name = "Nombre del Modelo"
+let ph_price = "Ingrese el precio"
+let ph_img= "Ingrese la imagen"
+let ph_archivo= "ingrese los archivos"
+
+
+</script>
+
+
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</svelte:head>
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Nombre Modelo</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Precio</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  
+  <div class="mb-3">
+    <label for="formFile" class="form-label">Imagen</label>
+    <input class="form-control" type="file" id="Imagen">
+  </div>
+  <div class="mb-3">
+    <label for="formFile" class="form-label">Archivo</label>
+    <input class="form-control" type="file" id="archivo">
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Vender</button>
+</form>
