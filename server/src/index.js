e9d302ba6671474e85c33ab8b381cddb16906e86
AnthonyLzq <sluzquinosa@uni.pe>
2022-07-15 18:45:32 -0500

    feat: cleaned entry point

--- server/src/index.js
+++ server/src/index.js
@@ -1,43 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const redis = require('redis')
-const connectRedis = require('connect-redis')
-const session = require('express-session')
-const apiRouter = require('./routes/api')
-const dotenv = require('dotenv')
+const morgan = require('morgan')
 
+const main = () => {
+  const app = express()
+  app.use(morgan("dev"))
+  app.use(cors())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  // app.use('/api', apiRouter)
 
-const morgan = require("morgan");
+  app.listen(3003, () => console.log('server running'))
+}
 
-//comentario del gabo 
-
-const app = express()
-const redisStore = connectRedis(session)
-dotenv.config()
-const redisClient = redis.createClient({
-  port: 6379,
-  host: 'localhost'
-})
-
-require('./db')
-app.use(morgan("dev"))
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-// app.use(session({
-//   store: new redisStore({
-//     client: redisClient,
-//     secret: 'obliteratorSecret',
-//     saveUninitialized: false,
-//     resave: false,
-//     cookie: {
-//       secure: false,
-//       httpOnly: true,
-//       maxAge: 1000 * 60 * 30 //30 minutes session 
-//     }
-//   })
-// }))
-app.use('/api', apiRouter)
-
-app.listen(3003, () => console.log('server running'))
+main()
