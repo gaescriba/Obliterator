33e6b3a48b1ba2ff00e157633103e1fd112c300c
AnthonyLzq <sluzquinosa@uni.pe>
2022-07-16 02:16:28 -0500

    feat: store 3d models route complete
    TODO:
      - move logic to service
      - complete the rest of the endpoints

--- server/src/network/routes/3dModels.js
+++ server/src/network/routes/3dModels.js
@@ -1,3 +1,4 @@
+import { getModels } from '../../database/index.js'
 import { multerInstance as multer, uploadImage } from '../../utils/index.js'
 
 /**
@@ -9,13 +10,14 @@ const api3dModelsRouter = (router, prefix = '/models') => {
   router.post(`${prefix}/`, multer.single('file'), async (req, res) => {
     try {
       const result = await uploadImage(req.file.path)
-      // const models = new Models({
-      //   model_name: req.body.model_name,
-      //   model_url: result.secure_url,
-      //   price: req.body.price
-      // })
+      const { Model3DModel } = getModels()
+      const model = await Model3DModel.create({
+        model_name: req.body.model_name,
+        image_url: result.secure_url,
+        price: parseInt(req.body.price)
+      })
 
-      res.json(result.secure_url)
+      res.json(model.get())
     } catch (error) {
       res.send(error)
     }
