aad0359ed20a506ce55a2be2224892f9c0d4eb8b
gaescriba <gaescriba.trabajo@gmail.com>
2022-07-16 23:32:53 -0400

    added gsap and tailwind

--- cliento/svelte.config.js
+++ cliento/svelte.config.js
@@ -1,16 +1,16 @@
-import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static'
-import WindiCSS from 'vite-plugin-windicss'
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [preprocess()],
-  kit: {
-    adapter: adapter(),
-    vite: () => ({
-      plugins: [WindiCSS()]
-    })
-  }
-}
+	preprocess: [
+		preprocess({
+		  postcss: true,
+		}),
+	  ],
+	kit: {
+		adapter: adapter()
+	}
+};
 
-export default config
+export default config;
